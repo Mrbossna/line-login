8939c90c78a9587389c16cf9563645583b7ef058
Mrbossna <bossza2468@gmail.com>
2024-08-13 16:59:08 +0000

    .env and email

--- sample-server.js
+++ sample-server.js
@@ -22,7 +22,7 @@ const login = new line_login({
     channel_id: process.env.LINE_LOGIN_CHANNEL_ID,
     channel_secret: process.env.LINE_LOGIN_CHANNEL_SECRET,
     callback_url: process.env.LINE_LOGIN_CALLBACK_URL,
-    scope: "openid profile",
+    scope: "openid profile email",
     prompt: "consent",
     bot_prompt: "normal"
 });
